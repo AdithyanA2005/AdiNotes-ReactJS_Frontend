260813a96452436252732634badc4e49ac06f94e
Adithyan A <deepaadithyan56@gmail.com>
2022-05-01 12:03:16 +0530

    NavLink Component added

---
+++ src/components/Navbar/NavLinks.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+export default function NavLinks(props) {
+  return (
+    <>
+      <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        {props.links.map((link, index) => (
+          <li key={index}>
+            <NavLink
+              to={link.url}
+              className={({ isActive }) => {
+                const commonClasses = "block py-2 pr-4 pl-3 rounded md:p-0" + " ";
+                return commonClasses.concat(
+                  isActive
+                    ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700  dark:text-white"
+                    : "text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                );
+              }}
+            >
+              {link.title}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,24 @@
 import { useEffect, useRef, useState } from "react";
+import { ReactComponent as BurgerIcon } from "../../assets/burger.svg";
+import { ReactComponent as CloseIcon } from "../../assets/close.svg";
 import NavBrand from "./NavBrand";
+import NavLinks from "./NavLinks";
 
 export default function Navbar() {
+  const [sidebarActive, setSidebarActive] = useState(false);
   const navbarRef = useRef();
+  const burgerRef = useRef();
+  const navigation = [{ title: "Home", url: "/" }];
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (burgerRef.current.contains(event.target)) return setSidebarActive((prev) => !prev);
+      return setSidebarActive(false);
+    };
+
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
 
   return (
     <>
@@ -12,6 +28,30 @@ export default function Navbar() {
       >
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <NavBrand />
+
+          <div className="flex md:order-2">
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Get started
+            </button>
+            <button
+              ref={burgerRef}
+              type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
+              <span className="sr-only">Open main menu</span>
+              {sidebarActive ? <CloseIcon /> : <BurgerIcon />}
+            </button>
+          </div>
+          <div
+            className={`${
+              sidebarActive ? "" : "hidden"
+            } justify-between items-center w-full md:flex md:w-auto md:order-1`}
+          >
+            <NavLinks links={navigation} />
+          </div>
         </div>
       </nav>
     </>
