28eafa6fe9cc5eb8734a24f5cc9a108a40ea9f4f
Adithyan A <deepaadithyan56@gmail.com>
2022-05-08 15:51:45 +0530

    Navbar Updaated with Get Start Link

--- src/components/Navbar/StartButton.js
+++ src/components/Navbar/StartButton.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../../context/Auth/AuthContext";
+import NoteFormContext from "../../context/NoteForm/NoteFormContext";
 
 export default function StartButton() {
+  const { auth } = useContext(AuthContext);
+  const { openNewNoteForm } = useContext(NoteFormContext);
+
   return (
     <>
       {/* TODO: Set this according to user | authenticated ? "create new Note" : "get started"  */}
-      <NavLink
-        to=""
-        className="hidden md:flex text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-[0.6rem] text-center mr-3 md:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
-      >
-        Get started
-      </NavLink>
+      {auth ? (
+        <button
+          onClick={openNewNoteForm}
+          className="hidden md:flex text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-[0.6rem] text-center mr-3 md:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+        >
+          Create New Note
+        </button>
+      ) : (
+        <NavLink
+          to="/auth/login"
+          className="hidden md:flex text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-[0.6rem] text-center mr-3 md:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+        >
+          Login To Account
+        </NavLink>
+      )}
     </>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,10 +4,12 @@ import StartButton from "./StartButton";
 export default function Navbar() {
   return (
     <>
-      <nav className="z-50 pl-16 dark:border-slate-700 border-b bg-white dark:bg-slate-900 h-16 grid place-items-center fixed top-0 right-0 left-0 transition-all ease-in-out duration-300 shadow-sm">
-        <div className="container px-10 flex items-center justify-center md:justify-between">
-          <NavBrand />
-          <StartButton />
+      <nav className="z-50 pl-16 h-16 fixed top-0 right-0 left-0 transition-all ease-in-out duration-300 shadow-sm">
+        <div className="h-full w-full grid place-items-center border-b dark:border-slate-700 bg-white dark:bg-slate-900 ">
+          <div className="container px-10 flex items-center justify-center md:justify-between">
+            <NavBrand />
+            <StartButton />
+          </div>
         </div>
       </nav>
     </>
