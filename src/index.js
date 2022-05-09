851918b52fab9fc47f8cba2e2f24fed4dea85a9e
Adithyan A <deepaadithyan56@gmail.com>
2022-05-09 22:01:51 +0530

    Updated with All Context State Applied

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import AuthState from "./context/Auth/AuthState";
+import NoteFormState from "./context/NoteForm/NoteFormState";
+import NoteState from "./context/Note/NoteState";
 import "./index.css";
+import LoaderState from "./context/Loader/LoaderState";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <LoaderState>
+    <AuthState>
+      <NoteState>
+        <NoteFormState>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </NoteFormState>
+      </NoteState>
+    </AuthState>
+  </LoaderState>
 );
