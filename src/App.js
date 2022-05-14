b8fc65d97555dce35f8b9a212a5992c62549f273
Adithyan A <deepaadithyan56@gmail.com>
2022-05-14 13:10:44 +0530

    Handle Routes for logged in user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import NoteState from "./context/Note/NoteState";
+import { useEffect, useState } from "react";
+import { Routes, Route, Redirect, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import Landing from "./pages/Landing";
+import React, { useContext } from "react";
+import AuthContext from "./context/Auth/AuthContext";
 import Main from "./pages/Main";
 
 export default function App() {
+  const { auth, setAuth } = useContext(AuthContext);
+
   useEffect(() => {
     document.documentElement.classList.add("scroll-smooth");
+
+    if (localStorage.authToken) {
+      setAuth({ authToken: localStorage.authToken });
+    }
   }, []);
 
   return (
     <>
-      <NoteState>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Main />} />
-          </Route>
-        </Routes>
-      </NoteState>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={!auth ? <Landing /> : <Main />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 }
