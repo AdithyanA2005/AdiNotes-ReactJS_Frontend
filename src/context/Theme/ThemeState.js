f008fb0917db4691d78bc2a3e71188731280faa8
Adithyan A <deepaadithyan56@gmail.com>
2022-05-18 18:31:03 +0530

    Color bug for autocompletion fixed

--- src/context/Theme/ThemeState.js
+++ src/context/Theme/ThemeState.js
@@ -3,12 +3,20 @@ import ThemeContext from "./ThemeContext";
 
 const ThemeState = (props) => {
   const [theme, setTheme] = useState("dark");
+  const autoCompleteInputTheme = (theme) => {
+    return document.documentElement.style.setProperty(
+      "--input-text-color",
+      `${theme == "dark" ? "rgb(203, 213, 225)" : "rgb(71, 85, 105)"}`
+    );
+  };
+
   useEffect(() => {
     localStorage.theme === "light" && setTheme("light");
   }, []);
 
   useEffect(() => {
     localStorage.theme = theme;
+    autoCompleteInputTheme(theme);
     if (theme === "dark") return document.documentElement.classList.add("dark");
     return document.documentElement.classList.remove("dark");
   }, [theme]);
