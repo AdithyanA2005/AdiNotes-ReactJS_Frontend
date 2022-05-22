63e0f35886fcc5f28a8e099dc93519031a4f96dc
Adithyan A <deepaadithyan56@gmail.com>
2022-05-22 10:27:04 +0530

    Sidebar button updated

--- src/components/Sidebar/SidebarButton.js
+++ src/components/Sidebar/SidebarButton.js
@@ -10,15 +10,19 @@ export default function SidebarButton({ children, tooltip, isBurger, sidebarActi
         {...props}
         className={`${
           isBurger
-            ? "h-11 w-11 bg-green-500 text-white hover:rounded-lg rounded-[0.9rem] flex items-center justify-center"
+            ? "h-11 w-11 bg-green-500 text-white hover:rounded-lg rounded-[0.7rem] flex items-center justify-center"
             : "h-12 dark:bg-slate-800 dark:text-green-500 dark:hover:bg-green-500 dark:hover:text-white text-green-500 bg-green-100 hover:bg-green-500 hover:text-white"
         } ${
           !isBurger &&
           (sidebarActive ? "rounded-r-3xl w-full pl-2" : "rounded-3xl w-12 hover:rounded-xl")
-        } group relative transition-all duration-300 ease-in-out cursor-pointer flex justify-start items-center shadow-lg`}
+        } group relative transition-all duration-300 ease-in-out cursor-poi nter flex justify-start items-center shadow-lg`}
       >
         {/* Children (Probably the Icon) */}
-        <span className={`${isBurger ? "h-11 w-11 p-3" : "h-12 w-12 p-3"} text-center  absolute `}>
+        <span
+          className={`${
+            isBurger ? "h-12 w-12 p-3" : "h-12 w-12 p-[0.825rem]"
+          } flex  items-center justify-center absolute`}
+        >
           {children}
         </span>
 
