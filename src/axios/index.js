6cd2f4ed1d7191fe768681f261784778150732ff
Adithyan A <deepaadithyan56@gmail.com>
2022-05-28 13:00:01 +0530

    Base url hidden

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getAxios = (authToken = null) =>
   axios.create({
-    baseURL: "<BACKEND_URL>",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
       "auth-token": authToken,
     },
