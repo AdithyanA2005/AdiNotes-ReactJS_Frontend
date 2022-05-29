f609ad8055766ac49d6384a5498e8b8fa1e3cbf4
Adithyan A <deepaadithyan56@gmail.com>
2022-05-29 23:30:04 +0530

    BUGFIX: Navburger button hovering over navbrand

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <>
       <nav className="z-40 h-16 fixed top-0 right-0 left-0 transition-all ease-in-out duration-300 shadow-sm">
         <div className="relative h-full w-full grid place-items-center border-b dark:border-slate-700 bg-white dark:bg-slate-900 ">
-          <div className="ml-16 container px-10 flex items-center justify-center md:justify-between">
+          <div className="px-10 md:px-14 md:ml-16 container  flex items-center justify-center md:justify-between">
             {/* Burger button */}
             <HamBurger />
 
